b341d561fc49a2e7a7795b92285abc76dd38f998
Boris Koumondji <brexis2009@yahoo.fr>
2019-02-10 11:11:12 +0000

    Add server broker

---
+++ src/ServerBrokerManager.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Brexis\LaravelSSO;
+
+use Brexis\LaravelSSO\Exceptions\InvalidSessionIdException;
+
+class ServerBrokerManager
+{
+    public function brokerModel()
+    {
+        $class = config('laravel-sso.brokers.model');
+
+        if (!class_exists($class)) {
+            throw new InvalidSessionIdException("Class $class does not exist");
+        }
+
+        return $class;
+    }
+
+    public function findBrokerById($id)
+    {
+        $class    = $this->brokerModel();
+        $id_field = config('laravel-sso.brokers.id_field');
+        $model    = $class::where($id_field, $id)->first();
+
+        if (!$model) {
+            throw new InvalidSessionIdException("Model $class with $id_field:$id not found");
+        }
+
+        return $model;
+    }
+
+    public function findBrokerSecret($model)
+    {
+        $secret_field = config('laravel-sso.brokers.secret_field');
+
+        return $model->$secret_field;
+    }
+
+    public function validateBrokerSessionId($sid)
+    {
+        $matches = null;
+
+        if (!preg_match('/^SSO-(\w*+)-(\w*+)-([a-z0-9]*+)$/', $sid, $matches)) {
+            throw new InvalidSessionIdException('Invalid session id');
+        }
+
+        $broker_id = $matches[1];
+        $token = $matches[2];
+
+        if ($this->generateSessionId($broker_id, $token) != $sid) {
+            throw new InvalidSessionIdException('Checksum failed: Client IP address may have changed');
+        }
+
+        return $broker_id;
+    }
+
+    public function generateSessionId($broker_id, $token)
+    {
+        $model  = $this->findBrokerById($broker_id);
+        $secret = $this->findBrokerSecret($model);
+        // TODO replace checksum by encription
+
+        return "SSO-{$broker_id}-{$token}-" . hash('sha256', 'session' . $token . $secret);
+    }
+
+    public function generateAttachChecksum($broker_id, $token)
+    {
+        $model  = $this->findBrokerById($broker_id);
+        $secret = $this->findBrokerSecret($model);
+        // TODO Replace checksum by encription
+
+        return hash('sha256', 'attach' . $token . $secret);
+    }
+
+    public function getBrokerSessionId($request)
+    {
+        $token = $request->bearerToken();
+
+        if (!$token) {
+            $token = $request->input('access_token');
+        }
+
+        if (!$token) {
+            $token = $request->input('sso_session');
+        }
+
+        return $token;
+    }
+}
---
+++ tests/ServerBrokerManagerTest.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Brexis\LaravelSSO\Test;
+
+use Brexis\LaravelSSO\ServerBrokerManager;
+use Brexis\LaravelSSO\Exceptions\InvalidSessionIdException;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Contracts\Cache\Store;
+use Illuminate\Http\Request;
+
+class ServerBrokerManagerTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->broker = new ServerBrokerManager();
+    }
+
+    public function testShouldThrownExceptionIfBrokerModelDoesNotExist()
+    {
+        $this->expectException(InvalidSessionIdException::class);
+        $this->expectExceptionMessage('Class Models\TestApp does not exist');
+
+        $this->app['config']->set('laravel-sso.brokers.model', 'Models\TestApp');
+
+        $this->broker->brokerModel();
+    }
+
+    public function testShouldNotThrownExceptionIfBrokerModelExists()
+    {
+        $this->app['config']->set('laravel-sso.brokers.model', Models\App::class);
+
+        $class = $this->broker->brokerModel();
+        $this->assertEquals($class, Models\App::class);
+    }
+
+    public function testShouldThrownExceptionIfModelDoesNotExist()
+    {
+        $this->expectException(InvalidSessionIdException::class);
+        $this->expectExceptionMessage('Class Models\TestApp does not exist');
+
+        $this->app['config']->set('laravel-sso.brokers.model', 'Models\TestApp');
+
+        $this->broker->findBrokerById(1);
+    }
+
+    public function testShouldNotThrownExceptionIfModelExists()
+    {
+        $this->app['config']->set('laravel-sso.brokers.model', Models\App::class);
+        $this->app['config']->set('laravel-sso.brokers.id_field', 'app_id');
+        $this->app['config']->set('laravel-sso.brokers.secret_field', 'secret');
+
+        Models\App::create(['app_id' => 'app-id', 'secret' => 'SeCrEt']);
+
+        $model = $this->broker->findBrokerById('app-id');
+        $this->assertEquals($model->app_id, 'app-id');
+
+        $secret = $this->broker->findBrokerSecret($model);
+        $this->assertEquals($secret, 'SeCrEt');
+    }
+
+    public function testShouldNotValidateSessionId()
+    {
+        $this->expectException(InvalidSessionIdException::class);
+        $this->expectExceptionMessage('Invalid session id');
+
+        $this->app['config']->set('laravel-sso.brokers.model', Models\App::class);
+        $this->app['config']->set('laravel-sso.brokers.id_field', 'app_id');
+        $this->app['config']->set('laravel-sso.brokers.secret_field', 'secret');
+
+        $this->broker->validateBrokerSessionId('SSO-FakeSessionID');
+    }
+
+    public function testShouldNotValidateChecksum()
+    {
+        $this->expectException(InvalidSessionIdException::class);
+        $this->expectExceptionMessage('Checksum failed: Client IP address may have changed');
+
+        $this->app['config']->set('laravel-sso.brokers.model', Models\App::class);
+        $this->app['config']->set('laravel-sso.brokers.id_field', 'app_id');
+        $this->app['config']->set('laravel-sso.brokers.secret_field', 'secret');
+
+        Models\App::create(['app_id' => 'appid', 'secret' => 'SeCrEt']);
+
+        $token = $this->generateToken();
+        $sid   = $this->generateSessionId('appid', $token, 'secret');
+
+        $this->broker->validateBrokerSessionId($sid);
+    }
+
+    public function testShouldValidateBrokerSessionId()
+    {
+        $this->app['config']->set('laravel-sso.brokers.model', Models\App::class);
+        $this->app['config']->set('laravel-sso.brokers.id_field', 'app_id');
+        $this->app['config']->set('laravel-sso.brokers.secret_field', 'secret');
+
+        Models\App::create(['app_id' => 'appid', 'secret' => 'SeCrEt']);
+
+        $token = $this->generateToken();
+        $sid   = $this->generateSessionId('appid', $token, 'SeCrEt');
+
+        $this->assertEquals($this->broker->validateBrokerSessionId($sid), 'appid');
+    }
+
+    public function testShouldReturnSessionIdFromRequest()
+    {
+        $request = new Request([], [], [], [], [], ['HTTP_AUTHORIZATION' => 'Bearer SeCrEt']);
+        $this->assertEquals($this->broker->getBrokerSessionId($request), 'SeCrEt');
+
+        $request = new Request(['access_token' => 'AccessToken']);
+        $this->assertEquals($this->broker->getBrokerSessionId($request), 'AccessToken');
+
+        $request = new Request([], ['sso_session' => 'SsoToken'], [], [], [], ['REQUEST_METHOD' => 'POST']);
+        $this->assertEquals($this->broker->getBrokerSessionId($request), 'SsoToken');
+    }
+}
